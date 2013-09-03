8da24478dc4a49823d9b155b42a4ad2485c18831
ivelinivanov <ivelin.ivanov@mentormate.com>
2013-09-03 13:18:32 +0300

    Initial Commit

---
+++ iMustache/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iMustache
+//
+//  Created by Ivelin Ivanov on 9/3/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iMustache/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iMustache
+//
+//  Created by Ivelin Ivanov on 9/3/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
